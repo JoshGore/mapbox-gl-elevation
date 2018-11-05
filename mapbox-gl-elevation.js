750af41d8365c4498ff6f4822f25eeb2e198a86c
Joshua Gore <Joshua.Gore8@gmail.com>
2018-11-05 15:34:10 +1030

    several options implemented

--- mapbox-gl-elevation.js
+++ mapbox-gl-elevation.js
@@ -1,6 +1,42 @@
 class ElevationControl {
 
-    constructor(feature) {
+    constructor(feature, options={}) {
+        /* options format
+         * {
+         * dimensions: {height: 15%, width: 15%}, (or as int for pixels)
+         * margins: {top: 0, left: 0, right: 0, bottom: 0},
+         * graphOffset: {height: 0, right: 0, left: 0},
+         * graphColor: 'lightsteelblue',
+         * graphLineColor: 'steelblue',
+         * graphOpacity: 1,
+         * icon: , // if not present no marker, if true default marker
+         * summary: false,
+         * }
+         */
+        this._options = {};
+        // create dimensions object if not in options
+        options.hasOwnProperty('dimensions') ? this._options.dimensions = options.dimensions : (this._options.dimensions = {});
+        // create margins object if not in options
+        options.hasOwnProperty('margins') ? this._options.margins = options.margins : (this._options.margins = {});
+        // define margins if not already set
+        !this._options.margins.hasOwnProperty('top') && (this._options.margins.height = 0);
+        !this._options.margins.hasOwnProperty('left') && (this._options.margins.height = 0);
+        !this._options.margins.hasOwnProperty('right') && (this._options.margins.right = 0);
+        !this._options.margins.hasOwnProperty('bottom') && (this._options.margins.left = 0);
+        // define graphOffset object if not already set
+        options.hasOwnProperty('graphOffset') ? this._options.graphOffset = options.graphOffset : (this._options.graphOffset = {});
+        // define individual values of graphOffset
+        !this._options.graphOffset.hasOwnProperty('height') && (this._options.graphOffset.height = 0);
+        !this._options.graphOffset.hasOwnProperty('left') && (this._options.graphOffset.height = 0);
+        !this._options.graphOffset.hasOwnProperty('right') && (this._options.graphOffset.right = 0);
+        // assign/define graph area color
+        this._options.graphColor = options.hasOwnProperty('graphColor') ? options.graphColor : 'lightsteelblue';
+        // assign/define graph area opacity
+        this._options.graphOpacity = options.hasOwnProperty('graphOpacity') ? options.graphOpacity : 1;
+        // assign/define graph line color
+        this._options.graphLineColor = options.hasOwnProperty('graphLineColor') ? options.graphLineColor : 'steelblue';
+        // assign icon if existing, true if default, leave if not defined
+        options.hasOwnProperty('icon') ? (this._options.icon = options.icon) : (this._options.icon = false);
         this._feature = feature;
         var vertices = turf.explode(feature);
         vertices.features.forEach(vertex => {
@@ -11,43 +47,73 @@ class ElevationControl {
                 feature["properties"]["distance"] = turf.distance(features[index - 1], feature) + features[index - 1].properties.distance;
         });
         this._vertices = vertices;
-        this._data = vertices.features.map(feature => [feature.properties.distance, feature.geometry.coordinates[2]]);
+        this._value = vertices.features.map(feature => [feature.properties.distance, feature.geometry.coordinates[2]]);
     };
 
     onAdd(map) {
         // store reference to map
         this._map = map;
+        // parse width and height options
+        if (this._options.dimensions.hasOwnProperty('width')) {
+            if (this._options.dimensions.width.charAt(this._options.dimensions.width.length - 1) == '%') {
+                this._options.dimensions.width = this._map._container.clientWidth * (Number(this._options.dimensions.width.slice(0, -1))/100);
+            }
+        }
+        else {
+            this._options.dimensions.width = this._map._container.clientWidth * 0.5; 
+        }
+        if (this._options.dimensions.hasOwnProperty('height')) {
+            if (this._options.dimensions.height.charAt(this._options.dimensions.height.length - 1) == '%') {
+                this._options.dimensions.height = this._map._container.clientHeight * (Number(this._options.dimensions.height.slice(0, -1))/100);
+            }
+        }
+        else {
+            this._options.dimensions.height = this._map._container.clientHeight * 0.5; 
+        }
+
+        // svgHeight to this._options.height
         // create div for control
         this._container = document.createElement('div');
         this._container.className = 'mapboxgl-ctrl';
         this._container.id = 'elevation-ctrl';
         // set a width for svg and parent container
-        var svgWidth = this._container.style.width = 500;
-        var svgHeight = this._container.style.height = 500;
+        this._container.style.width = this._options.dimensions.width;
+        this._container.style.height = this._options.dimensions.height;
         // set margin and dimensions for svg child
-        var margin = { top: 20, right: 20, bottom: 30, left: 50  };
-        var width = svgWidth - margin.left - margin.right;
-        var height = svgHeight - margin.top - margin.bottom;
+        // var margin = { top: 20, right: 20, bottom: 30, left: 50  };
+        var margin = this._options.margins;
+        var width = this._options.dimensions.width - margin.left - margin.right;
+        var height = this._options.dimensions.height - margin.top - margin.bottom;
         // define scale, dimensions of svg to scale data to and domains, range of data values
         this._x = d3.scaleLinear().range([0, width]).interpolate(d3.interpolateRound)
-            .domain(d3.extent(this._data, d => d[0]));
-        this._y = d3.scaleLinear().range([height, 0]).interpolate(d3.interpolateRound)
-            .domain(d3.extent(this._data, d => d[1]));
+            .domain(d3.extent(this._value, d => d[0]));
+        this._y = d3.scaleLinear().range([(height), 0]).interpolate(d3.interpolateRound)
+            .domain(d3.extent(this._value, d => d[1]));
         // select parent element
         var element = d3.select(this._container);
         // create svg container and set height/width attributes
         var svg = element.append('svg').style('display', 'block');
-        svg.attr("width", svgWidth).attr("height", svgHeight);
+        svg.attr("width", this._options.dimensions.width).attr("height", this._options.dimensions.height);
         // create group to put plot within, translated by margins, dimensions less margins
-        var g = svg.append("g")
-            .attr("height", height)
+        var graphs = svg.append("g")
+            .attr("height", (height))
+            .attr("width", width)
+            .attr("transform", `translate(${margin.left}, ${margin.top})`);
+        // create overlay group
+        var overlay = svg.append("g")
+            .attr("height", (height))
             .attr("width", width)
             .attr("transform", `translate(${margin.left}, ${margin.top})`);
         // add marker to map at start of path
-        this._marker = new mapboxgl.Marker().setLngLat(this._vertices.features[0].geometry.coordinates).addTo(this._map);
+        if (this._options.icon) {
+            this._marker = new mapboxgl.Marker(!(this._options.icon === true) && this._options.icon).setLngLat(this._vertices.features[0].geometry.coordinates).addTo(this._map);
+        }
+        else {
+            this._marker = false;
+        }
         // then draw chart in group
-        this.drawChart(this._data, g);
-        this.addOverlay(this._data, g);
+        this.drawChart(this._value, graphs);
+        this.addOverlay(this._value, overlay);
         return this._container;
     };
     
@@ -64,8 +130,10 @@ class ElevationControl {
         let line = d3.line()
             .x(d => this._x(d[0]))
             .y(d => this._y(d[1]));
+        // create group for areas
+        const areas = g.append('g').attr("opacity", this._options.graphOpacity);
         // add polygon area path to group with predefined data and function
-        g.append("path")
+        areas.append("path")
             .datum(data)
             .attr("class", "area")
             .attr("d", area);
@@ -77,10 +145,10 @@ class ElevationControl {
         // style line and area
         g.selectAll('.line')
             .style('fill', 'none')
-            .style('stroke', 'steelblue')
+            .style('stroke', this._options.graphLineColor)
             .style('stroke-width', '1.5px');
         g.selectAll('.area')
-            .style('fill', 'lightsteelblue')
+            .style('fill', this._options.graphColor)
     };
 
     addOverlay(data, g) {
@@ -155,7 +223,7 @@ class ElevationControl {
                 .attr('x2', 0)
                 .attr('y1', 0)
                 .attr('y2', height - y(d[1]));
-            setMarkerByDistance(lineFeature, d[0], marker)
+            marker && setMarkerByDistance(lineFeature, d[0], marker)
         };
     }
 
